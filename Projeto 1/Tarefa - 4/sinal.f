ac9f86c8a7740a6c7d201e882f08ca53983a2f87
Edmur C. Neto <117097092+Edmurcn@users.noreply.github.com>
2023-07-23 12:12:26 -0300

    Update Projeto1

---
+++ Projeto 1/Tarefa - 4/sinal.f
@@ -0,0 +1,26 @@
+      implicit Real*8 (a-h,o-z)
+      parameter (pi = acos(-1.d0))
+
+      open(1,file = "data.in")
+
+      !Importando parâmetros do sinal
+
+      read(*,*) N, a1, a2, w1, w2, dt
+      write(*,*) "N, a1, a2, w1, w2, dt"
+      
+      w1 = w1*pi
+      w2 = w2*pi
+        
+      t = 0.d0
+
+      !Construção do sinal
+
+      do i= 0, (N-1)
+         t = dt*i
+         y1 = a1*dcos(w1*t) + a2*dsin(w2*t)
+         write(1,*) t, y1
+      enddo
+
+      close(1)
+
+      end Program
