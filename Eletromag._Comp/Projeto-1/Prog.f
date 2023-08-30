2c62f27414df0b13e1a02d2a94f1b834e1f95716
Edmur C. Neto <117097092+Edmurcn@users.noreply.github.com>
2023-08-30 11:03:31 -0300

    update Projeto-1

--- Eletromag._Comp/Projeto-1/Prog.f
+++ Eletromag._Comp/Projeto-1/Prog.f
@@ -1,44 +1,64 @@
 	implicit real*4 (a-h, o-z)
-	parameter (L = 5)
+	parameter (L = 100, L2 = 20)
 	dimension v(0:L, 0:L), v_f(0:L, 0:L)
-	parameter (erro = 10e-6)
+	parameter (erro = 10e-8)
+	
+	open(2, file = "dados.dat")
 
 	v = 0.e0
-	v_f = 0.e0
-	soma = 0.e0
-	count = 0.e0
+	count_ = 0.e0
 	dif = 1.e0
 
-	do i = 0, 2
-		do j = 0, 2
+	do i = 0, L2
+		do j = 0, L2   
 			v(i,j) = 1.e0
 		enddo
 	enddo
-
-	do while(dif.gt.erro)
+	
+	v_f = v
+	
+	do while(dif.gt.erro*count_)
 
 		do i = 0,L
 			do j = 0,L
-				write(1,*) v(i,j)
+				write(1,2) i, j, v(i,j)
+2				format(I3, I4, F6.3)		
 			enddo
 		enddo
-
-		do j = 2, L-1
-			do i = 2, L-1
-
-				v_f(i,j) = (v(i-1, j) + v(i+1, j) + 
+		
+		soma = 0.e0
+		
+		do j = 0, L-1
+			do i = 0, L-1
+				
+				if (i.le.L2.and.j.le.L2) then
+					v_f(i,j) = 1.e0
+					exit
+				else
+					v_f(i,j) = (v(i-1, j) + v(i+1, j) + 
      &v(i, j-1) + v(i, j+1))/4.e0
 				
-				soma = soma + v_p(i,j) - v(i,j)
-				count = count + 1.e0
+					soma = soma + abs(v(i,j) - v_f(i,j))
+					count_ = count_ + 1.e0
+				endif
+
 			enddo
 		enddo
 
-		dif = soma/count	
+		dif = soma
 		
 		write(1,*) ""
 		write(1,*) ""
 
+		v = v_f
+
+	enddo
+	
+	do i=0,L
+		do j=0,L
+			write(2,1) i, j, v(i,j)
+1			format(I3, I4, F6.3)	
+		enddo
 	enddo
 			
 	end 
