7029ad387f840796673d7cda6d7b6fb3915c6d25
Edmur C. Neto <117097092+Edmurcn@users.noreply.github.com>
2023-12-04 12:18:33 -0300

    Update Projeto-3

--- Eletromag._Comp/Projeto 3/6.4/Prog.f
+++ Eletromag._Comp/Projeto 3/6.4/Prog.f
@@ -34,7 +34,27 @@
      $(y(i+1,n)+y(i-1,n))-y(i,n-1)
      
      		write(1,*)  i*dx, y(i,n)
-     	
+     		if (n.eq.0) then
+     			write(11,*) i*dx, y(i,n+1)-n/20.e0
+  		endif
+     		if (n.eq.10) then
+     			write(2,*) i*dx, y(i,n+1)-n/20.e0
+  		endif
+  		if (n.eq.30) then
+     			write(3,*) i*dx, y(i,n+1)-n/20.e0
+  		endif
+  		if (n.eq.50) then
+     			write(4,*) i*dx, y(i,n+1)-n/25.e0
+  		endif
+  		if (n.eq.80) then
+     			write(8,*) i*dx, y(i,n+1)-n/25.e0
+  		endif
+  		!if (n.eq.110) then
+     		!	write(9,*) i*dx, y(i,n+1)+n/5.e0
+  		!endif
+  		!if (n.eq.150) then
+     		!	write(10,*) i*dx, y(i,n+1)+n/5.e0
+  		!endif
 		enddo
 		
 		write(1,*) 1, y(100,n)
